9879b049a7563dd6232b11553669841c6e3253c2
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-30 18:09:06 -0400

    Merge branch 'baseline' of https://github.com/bahrus/be-calculating into baseline

--- be-calculating.js
+++ be-calculating.js
@@ -1,3 +1,15 @@
 // @ts-check
 import { resolved, rejected, propInfo} from 'be-enhanced/cc.js';
 import { BE } from 'be-enhanced/BE.js';
+
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
+/** @import {Actions, PAP,  AP} from './types.d.ts' */;
+/** @import {EnhancementInfo} from './ts-refs/trans-render/be/types.d.ts' */
+
+/**
+ * @implements {Actions}
+ * 
+ */
+class BeCalculating extends BE {
+    
+}
