acb4298c74ce137c5d7383eac19e92fa26f7db74
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-09-10 07:22:29 -0400

    0.0.29

--- be-calculating.js
+++ be-calculating.js
@@ -8,6 +8,7 @@ import { propInfo } from 'be-enhanced/cc.js';
 /** @import {AbsorbingObject} from './ts-refs/trans-render/asmr/types.d.ts' */
 /** @import {AllProps as BeExportableAllProps} from  './ts-refs/be-exportable/types.d.ts' */
 
+let cnt = 0;
 /**
  * @implements {Actions}
  * @implements {EventListenerObject}
@@ -136,8 +137,80 @@ class BeCalculating extends BE {
         });
     }
 
+    /**
+     * 
+     * @param {BAP} self 
+     */
+    async seek(self){
+        const {remoteSpecifiers, enhancedElement, defaultEventType} = self;
+        const {find} = await import('trans-render/dss/find.js');
+        const {ASMR} = await import('trans-render/asmr/asmr.js');
+        //const eventTypeToListen = publishEventType !== 'load' ? publishEventType || 'input' : 'input';
+        /**
+         * @type {{[key: string]: AbsorbingObject}}
+         */
+        const propToAO = {};
 
+        for(const remoteSpecifier of remoteSpecifiers){
+            const remoteEl = await find(enhancedElement, remoteSpecifier);
+            if(!(remoteEl instanceof Element)) continue;
+            if(enhancedElement instanceof HTMLOutputElement && !remoteEl.id){
+                const id = `be-calculating-${cnt}`;
+                remoteEl.id = id;
+                enhancedElement.htmlFor.add(id);
+                cnt++;
+            }
+            const {prop} = remoteSpecifier;
+            if(prop === undefined) throw 'NI';
+            const ao = await ASMR.getAO(remoteEl, {
+                evt: remoteSpecifier.evt || defaultEventType,
+                selfIsVal: remoteSpecifier.path === '$0',
+            });
+            propToAO[prop] = ao;
+        }
+        return {
+            propToAO
+        };
+    }
+    /**
+     * @type {AbortController | undefined}
+     */
+    #ac
+    /**
+     * 
+     * @param {BAP} self 
+     */
+    async hydrate(self){
+        this.disconnect();
+        const {propToAO} = self;
+        const aos = Object.values(propToAO);
+        for(const ao of aos){
+            const ac = this.#ac = new AbortController();
+            ao.addEventListener('.', this, {signal: ac.signal});
+
+        }
+        this.handleEvent();
+        return {
+            resolved: true
+        }
+    }
     async handleEvent() {
+
+    }
+
+        /**
+     * @param {Element} enhancedElement
+     * @override
+     */
+    async detach(enhancedElement){
+        await super.detach(enhancedElement);
+        this.disconnect();
+    }
+    
+    disconnect(){
+        if(this.#ac !== undefined){
+            this.#ac.abort();
+        };
 
     }
 }
