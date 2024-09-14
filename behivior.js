56347218024ba5b6e4a2686d58903f7bdd19ff12
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-09-14 15:16:06 -0400

    0.0.31

--- behivior.js
+++ behivior.js
@@ -1,6 +1,7 @@
 // @ts-check
 import { BeHive, seed, MountObserver } from 'be-hive/be-hive.js';
 import {Registry} from 'be-hive/Registry.js';
+import {aggs} from 'be-hive/aggEvt.js';
 /** @import {EMC, EventListenerOrFn} from './ts-refs/trans-render/be/types.d.ts' */
 /** @import {Actions, PAP,  AP} from './ts-refs/be-calculating/types' */;
 /** @import {CSSQuery} from './ts-refs/trans-render/types.js' */
@@ -45,10 +46,9 @@ export const emc = {
 const mose = seed(emc);
 MountObserver.synthesize(document, BeHive, mose);
 
-Registry.register(emc, '+', e => e.r = e.args.reduce((acc, arg) => acc + arg));
-Registry.register(emc, '*', e => e.r = e.args.reduce((acc, arg) => acc * arg));
-Registry.register(emc, 'max', e => e.r = e.args.reduce((acc, arg) => Math.max(acc, arg)));
-Registry.register(emc, 'min', e => e.r = e.args.reduce((acc, arg) => Math.min(acc, arg)));
+for(const key in aggs){
+    Registry.register(emc, key, aggs[key]);
+}
 
 /**
  * 
